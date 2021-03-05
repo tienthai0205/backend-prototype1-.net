6feb26b958d227ec99d3910f626bb2a2f0bf3430
Thai Thuy Tien <tienthai0205>
2021-03-05 19:56:27 +0100

    handle wrong password enter

--- ReservationApi/Controllers/UsersController.cs
+++ ReservationApi/Controllers/UsersController.cs
@@ -66,7 +66,7 @@ namespace ReservationApi.Controllers
 
             if (!verified)
             {
-
+                return BadRequest(new { error = "Username or password incorrect" });
             }
             var claims = new[]
             {
